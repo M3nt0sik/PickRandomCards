f1c1e50b77e73050f1c17f29aa7df04bb44e67c4
Kamil <mentos.96@o2.pl>
2023-05-06 17:06:27 +0200

    suplemented main program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using PickRandomCards;
+
+Console.Write("Wpizs liczbę generowanych kart: ");
+string line = Console.ReadLine();
+if(int.TryParse(line ,out int numberOfCards))
+{
+    string[] tab = CardPicker.PickSomeCards(numberOfCards);
+    foreach(string card in tab)
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine("Incorect value");
+}
